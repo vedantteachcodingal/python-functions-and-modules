2340161e8cbd7c8016c6068f8415365aa2ed121c
vedantteachcodingal <vedant.teach.codingal@gmail.com>
2026-01-04 21:07:13 +0530

    complete training

--- acp.py
+++ acp.py
@@ -1,4 +1,4 @@
-## function
+## lesson 1 - function
 # question - define a function called my_second_function. this function should print "this is my second function" when called. call this function outside of its definition
 # def my_second_function():
 #     print("this is my second function")
@@ -6,7 +6,7 @@
 # my_second_function()
 
 
-## arguments
+## lesson 2 - arguments
 # question - define a function that finds the circumference of a circle. The function should take in the radius of the circle as an argument. and then the function should return that circumference value rounded to 2 decimal places
 # def circumference(radius):
 #     circumference = 2 * 3.14 * radius
@@ -15,20 +15,31 @@
 
 # print(circumference(5))
 
-## keywords
-# question - write a function called check_prime. this function should take in a number as an argument. this function returns True if the number is prime, and returns False otherwise. call this function again and again until a prime number is entered by the user. no need to use recursion here
-# def check_prime(num):
-#     for i in range(2, num):
-#         if num % i == 0:
-#             return False
-#     return True
+## lesson 3 - keywords
+# question 1 - write a function called check_prime. this function should take in a number as an argument. this function returns True if the number is prime, and returns False otherwise. call this function again and again until a prime number is entered by the user. no need to use recursion here
+def check_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
 
-# while True:
-#     n = int(input("Enter a prime number: "))
-#     if check_prime(n):
-#         break
-    
-## exception handling
+while True:
+    n = int(input("Enter a prime number: "))
+    if check_prime(n):
+        break
+
+# question 2 (optional) - take a natural number from user. print the sum of all natural numbers from 1 till the entered natural number. use recursion to find the sum
+# def sum(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n + sum(n - 1) 
+
+# answer = sum(5)
+# print(answer)
+
+
+## lesson 4 - exception handling
 # question - read about generic exception handling in google. demonstrate this in next class
 # try:
 #     int("abc")
@@ -37,9 +48,32 @@
 # except Exception as e: # it is best practice to catch specific errors wherever possible, and use generic exception only as a fallback
 #     print("Oops!:", e)
 
-## random and math module
+## lesson 5 - random and math module
 # question - use appropriate function from the math module to find the cube root of a given number. use google/chatgpt
-import math
-n = math.cbrt(27)
-print(n)
+# import math
+# n = math.cbrt(27)
+# print(n)
 
+## lesson 6 - datetime and calendar module
+# question 1 - 
+import calendar
+
+for month_num in range(1, 13):
+    print(calendar.month_name[month_num])
+
+
+# question 2 - print a random time (HH:SS) within the range from start time (HH:SS) to end time (HH:SS)
+# not reviewed
+import random
+import datetime
+
+start_time = datetime.datetime.strptime("09:00", "%H:%M")
+end_time   = datetime.datetime.strptime("10:00", "%H:%M")
+
+total_minutes = int((end_time - start_time).total_seconds() / 60)
+
+random_minutes = random.randint(0, total_minutes)
+
+random_time = start_time + datetime.timedelta(minutes=random_minutes)
+
+print("Random time:", random_time.strftime("%H:%M"))
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-## function
-# def my_first_function():
-#     print("this is my first function")
+## lesson 1 - function
+def my_first_function():
+    print("this is my first function")
 
-# my_first_function()
+my_first_function()
 
-## arguments
-# question - define a function that finds the double of a number. The function should take in the number as an argument. and then the function should return that doubled value.
+## lesson 2 - arguments
+# activity 1 - define a function that finds the double of a number. The function should take in the number as an argument. and then the function should return that doubled value
 # def double(x):
-#     return x * 2
+#     result = x * 2
+#     return result
 
 # answer = double(5)
 # print(answer)
 
-## keywords
+## lesson 3 - keywords
 # activity 1 - write a function called check_prime. this function should take in a number as an argument. this function should return True if the number is prime, and False otherwise
 # def check_prime(num):
 #     for i in range(2, num):
@@ -46,7 +47,7 @@
 # answer = factorial(5)
 # print(answer)
 
-## exception handling
+## lesson 4 - exception handling
 # activity 1 - ask the user to enter their age. if the age is 18 or more, print "You are an adult", otherwise print "You are a minor". if the user enters a non-numeric value, print "Error: Please enter a valid number!"
 # try:
 #     age = int(input("Enter your age: "))
@@ -56,6 +57,8 @@
 #         print("You are a minor")
 # except ValueError:
 #     print("Error: Please enter a valid number!")
+
+# print("other coding")
 
 # activity 2 - this activity is almost the same as activity 1, but here you have to keep asking the user to enter their age until they enter a valid number
 # while True:
@@ -69,7 +72,7 @@
 #     except ValueError:
 #         print("Error: Please enter a valid number!")
 
-## random and math module
+## lesson 5 - random and math module
 # activity 1 - demonstration of random module
 # import random
 # n = random.randint(1, 10) # includes both 1 and 10
@@ -110,18 +113,52 @@
 #     print("Computer wins!")    
 
 # activity 3 - demonstration of math module
-import math
-n = math.sqrt(19)
-n = round(n, 2)
-print(n)
+# import math
+# n = math.sqrt(19)
+# n = round(n, 2)
+# print(n)
 
-f = math.floor(3.8)
-print(f)
-c = math.ceil(3.2)
-print(c)
+# f = math.floor(3.8)
+# print(f)
+# c = math.ceil(3.2)
+# print(c)
 
-lcm = math.lcm(40, 8)
-print(lcm)
+# lcm = math.lcm(40, 8)
+# print(lcm)
 
-## date, time and calendar module
-#         
+## lesson 6 - datetime and calendar module
+# activity 1 - demonstration of datetime module
+# import datetime
+
+# now = datetime.datetime.today()
+# print(type(now))
+# print("current date and time:", now)
+# print("current year:", now.year)
+# print("curent month:", now.month)
+# print("current day:", now.day)
+# print("current hour:", now.hour)
+# print("current minute:", now.minute)
+
+# aciivity 2 - print a random date within the range from start date to end date
+# import datetime
+# import random
+
+# start_date = datetime.date(2023, 1, 1) # because for the date object the constructor's signature is (year, month, day)
+# end_date = datetime.date(2025, 12, 31)
+# print(start_date)
+# delta = end_date - start_date # delta is an object of class timedelta 
+# print(delta)
+# print(delta.days)
+# random_days = random.randint(0, delta.days)
+# random_date = start_date + datetime.timedelta(days=random_days) # timedelta class is for duration
+# print("Random date: ", random_date)
+# print(datetime.timedelta(days=20))
+
+# activity 3 - check for a year to be leap year or not, using calendar module
+# import calendar
+# year = int(input("Enter a year: "))
+# print(calendar.isleap(year))
+# if calendar.isleap(year):
+#     print("It's a leap year")
+# else:
+#     print("It's not a leap year")
